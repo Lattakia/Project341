8a1089957cf907b94b159d670f4f7c0478bce5ef
Lattakia <hannageorgi@hotmail.com>
2017-02-13 15:40:38 -0500

    Update passport.js

--- passport.js
+++ passport.js
@@ -48,5 +48,47 @@ module.exports = function(passport) {
         });
 
     }));
+    
+    passport.use('local-signup', new LocalStrategy({
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true 
+    },
+    function(req, email, password, done) {
+
+        process.nextTick(function() {
+
+     
+        User.findOne({ 'local.email' :  email }, function(err, user) {
+            // if there are any errors, return the error
+            if (err)
+                return done(err);
+
+            // verify if there is already a user with that email
+            if (user) {
+                return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+            } else {
+
+                // if there is no user with that email
+                // create the user
+                var newUser            = new User();
+
+                // set the user's local credentials
+                newUser.local.email    = email;
+                newUser.local.password = newUser.generateHash(password);
+
+                // save the user
+                newUser.save(function(err) {
+                    if (err)
+                        throw err;
+                    return done(null, newUser);
+                });
+            }
+
+        });    
+
+        });
+
+    }));
 
 };
