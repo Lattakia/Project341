bb4a58088a6e311d1d2b2c02b018a46c55713ac9
fatin123 <hosfatin@gmail.com>
2017-03-09 02:43:05 -0500

    Modified js to apply to the new profile page

--- scripts/inputRestriction.js
+++ scripts/inputRestriction.js
@@ -1,48 +1,28 @@
 $(document).ready(function(){
 
-
-
 $(".integer").numeric(false, function() { alert("Integers only"); this.value = ""; this.focus(); });
 
-
-/*
-$("#SaveButton").click(function(){
-
-	//var numChar = $("#NewPass").text().length();
-	alert("Your password is " +  numChar + " characters.");
-
-});
-*/
-
-/*
-$("#SaveButton").on( "click", function() {
-	
-	alert("Hi");
-	//var numChar = $("#NewPass").text().length();
-	//alert("Your password is " +  numChar + " characters.");
-});
-*/
-
-
 $("#buttons").on("click", "#SaveButton", function(){
+	
     
-	var oldPassChar = $("#OldPass").val();
-	var newPassChar = $("#NewPass").val();
-	var newPassCChar = $("#NewPassC").val();
-	var newMessage = "";
-	if(newPassChar == oldPassChar){
-		newMessage += "- New password is equal to the old password!\n";
+	var emailText = $("#email").val();
+	
+	if( !isEmail(emailText)  ){
+		
+			alert("Invalid Email");
+	}else{
+		//SAVE TO DATABASE
+		alert("TO DO: Now save to database and refresh page");
 	}
-	if(newPassChar.length < 8){
-		newMessage += "- Your password is only " +  newPassChar + " characters. Minimum is 8.\n";
-	}
-	if(newPassCChar != newPassChar){
-		newMessage += "- Your confirm password is not equal to the new password.\n";
-	}
-	alert(newMessage);
+	
+	
+	
 });
 
 
-
-
 });
+
+function isEmail(email) {
+  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  return regex.test(email);
+}
