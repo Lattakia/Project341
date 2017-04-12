8cacdf9792caca4f1f5018b6a3fb55ff5a7bec18
Andrew <awfeng1994@hotmail.com>
2017-04-12 03:22:30 -0400

    small successful test

---
+++ unit_test/routeTest.js
@@ -0,0 +1,14 @@
+/* Dependencies */
+var chai = require('chai');
+
+/* Assertion Styles */
+var assert = chai.assert;
+var expect = chai.expect;
+
+/* Tests */
+describe('Test', function() {
+	it('should be successful', function(){
+		var value = 0;
+		expect(value).to.equal(0);
+	});
+});
