678e0d6cdf23b5454a3cba8b97a8ab6fcdd07c9b
Andrew <awfeng1994@hotmail.com>
2017-04-12 18:05:05 -0400

    updated the testing for checkAuthentication

--- unit_test/routeTest.js
+++ unit_test/routeTest.js
@@ -1,9 +1,13 @@
 /* Dependencies */
 var chai = require('chai');
+var sinon = require('sinon');
+var routes = require('../routes.js');
 
 /* Assertion Styles */
 var assert = chai.assert;
 var expect = chai.expect;
+
+/* Specifying function variable */
 
 /* Tests */
 describe('Test', function() {
@@ -11,4 +15,20 @@ describe('Test', function() {
 		var value = 0;
 		expect(value).to.equal(0);
 	});
+
+	it('should call next() if the authentication succeeds', function(){
+		var request = {
+			isAuthenticated : function() {return true;}
+		},
+			response = {
+				redirect : function(path) { console.log(path);}
+			},
+			next = function(err) {
+				console.log('hi')
+			};
+
+		routes.checkAuthentication(request, response, next);
+	});
 });
+
+
